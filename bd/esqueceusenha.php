99daf3a1807d98797909e9a5c784159d87135ee0
Igor <igormorato137@email.com>
2026-04-30 15:18:45 -0300

    Atualizações no projeto

---
+++ bd/esqueceusenha.php
@@ -0,0 +1,13 @@
+<?php
+    $conexao = mysqli_connect("localhost", "root", "", "bdlogin");
+    $email = $_POST['email'];
+    $password_new = $_POST['password'];
+
+    $sql = "UPDATE tblogin SET senha = '$password_new' WHERE email = '$email'";
+    if (mysqli_query($conexao, $sql)) {
+        header("Location: ../index.html");
+        exit;
+    } else {
+        echo "Erro ao criar conta: " . mysqli_error($conexao);
+    }
+?>
