b8f076122fd342e81d4db2e171430cf82841c183
Igor <igormorato137@email.com>
2026-04-30 19:09:31 -0300

    Atualizações no projeto

--- bd/bd.php
+++ bd/bd.php
@@ -5,12 +5,16 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $sql = "SELECT * FROM tblogin WHERE email='$email' AND senha='$password'";
+if (empty($email) || empty($password)) {
+    header("Location: ../index.html?error=emptyfields");
+    exit;
+}
 $result = mysqli_query($conexao, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     header("Location: ../home.html");
     exit;
 } else {
-    echo "Email ou senha incorretos.";
+    header("Location: ../index.html?error=invalidcredentials");
+    exit;
 }
 ?>
